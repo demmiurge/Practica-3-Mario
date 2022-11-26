8b0e741b1c98b5c0721869c401123ca79152d5ad
GuillemExe <guillem.arrojo@gmail.com>
2022-11-26 19:30:36 +0100

    Coins

--- Assets/Code/Coin/CoinValue.cs
+++ Assets/Code/Coin/CoinValue.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CoinValue : MonoBehaviour
 {
@@ -15,11 +16,23 @@ public class CoinValue : MonoBehaviour
     public List<int> m_ValueCoins;
     public List<Material> m_MaterialsCoins;
 
+    [Header("Manager NEED REFACTOR")] 
+    public CoinStarManager m_CoinStarManager;
+
     // Start is called before the first frame update
     void Start()
     {
         m_CoinValue = m_ValueCoins[(int)m_TypeCoin];
         m_CoinMaterial.GetComponent<Renderer>().material = m_MaterialsCoins[(int)m_TypeCoin];
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_CoinStarManager.AddCoinds(m_CoinValue);
+            gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/Code/Manager/CoinStarManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CoinStarManager : MonoBehaviour
+{
+    private int m_Coins;
+    private int m_Stars;
+
+    [Header("UI")]
+    public GameObject m_TextCoins;
+    public GameObject m_TextStars;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Coins = 0;
+        m_Stars = 0;
+        UpdateCoins();
+        UpdateStars();
+    }
+
+    public void AddCoinds(int l_Coins)
+    {
+        m_Coins += l_Coins;
+        UpdateCoins();
+    }
+
+    public void AddStars(int l_Stars)
+    {
+        m_Stars += l_Stars;
+        UpdateStars();
+    }
+
+    void UpdateCoins()
+    {
+        m_TextCoins.GetComponent<TextMeshPro>().text = m_Coins.ToString();
+    }
+
+    void UpdateStars()
+    {
+        m_TextStars.GetComponent<TextMeshPro>().text = m_Stars.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
