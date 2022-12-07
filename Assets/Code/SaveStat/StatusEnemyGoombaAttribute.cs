2ea62198c6fa6ffc8b0fa864aa680777ffe59b79
GuillemExe <guillem.arrojo@gmail.com>
2022-12-07 16:31:12 +0100

    Fix

--- Assets/Code/SaveStat/StatusEnemyGoombaAttribute.cs
+++ Assets/Code/SaveStat/StatusEnemyGoombaAttribute.cs
@@ -14,6 +14,9 @@ public class StatusEnemyGoombaAttribute : GameObjectStateLoadReload
     bool m_IsDead;
     bool m_IsDeadInitial;
 
+    Vector3 m_Scale;
+    Vector3 m_ScaleInitial;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,26 +25,31 @@ public class StatusEnemyGoombaAttribute : GameObjectStateLoadReload
         SetCurrentAttributesAsDefault();
         m_CurrentPunchesToKillInitial = m_GoombaEnemy.GetCurrentPunches();
         m_IsDeadInitial = m_GoombaEnemy.GetIsDead();
+        m_ScaleInitial = m_GoombaEnemy.gameObject.transform.localScale;
     }
 
     public override void SetCurrentAttributesAsDefault()
     {
         m_CurrentPunchesToKill = m_GoombaEnemy.GetCurrentPunches();
         m_IsDead = m_GoombaEnemy.GetIsDead();
+        m_Scale = m_GoombaEnemy.gameObject.transform.localScale;
     }
 
     public override void LoadDefaultAttributes()
     {
         m_GoombaEnemy.SetCurrentPunches(m_CurrentPunchesToKill);
         m_GoombaEnemy.SetIsDead(m_IsDead);
+        m_GoombaEnemy.transform.localScale = m_Scale;
     }
 
     public override void ResetDefaultAttributes()
     {
         m_CurrentPunchesToKill = m_CurrentPunchesToKillInitial;
         m_IsDead = m_IsDeadInitial;
+        m_Scale = m_ScaleInitial;
 
         m_GoombaEnemy.SetCurrentPunches(m_CurrentPunchesToKillInitial);
         m_GoombaEnemy.SetIsDead(m_IsDeadInitial);
+        m_GoombaEnemy.transform.localScale = m_ScaleInitial;
     }
 }
